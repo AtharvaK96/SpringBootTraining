aca608e3b67d12181195afce823ebb69ecd64228
akshayv <akshayvaidya@benchmarkit.solutions>
2024-08-28 14:19:58 +0530

    added jdbc code

---
+++ SpringDataJPA/JdbcTempleteTraining/src/main/java/h2JDBCTraining/JdbcTempleteTraining/JdbcTempleteTrainingApplication.java
@@ -0,0 +1,13 @@
+package h2JDBCTraining.JdbcTempleteTraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcTempleteTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcTempleteTrainingApplication.class, args);
+	}
+
+}
---
+++ SpringDataJPA/JdbcTempleteTraining/src/test/java/h2JDBCTraining/JdbcTempleteTraining/JdbcTempleteTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package h2JDBCTraining.JdbcTempleteTraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcTempleteTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
