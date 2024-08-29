0bda52a317a9075fdaedae8af80d33aed3391717
Heena <dongarkarheena978@gmail.com>
2024-08-29 16:57:24 +0530

    Successfully committed

---
+++ SpringDataJPA/assignment_four_new/src/main/java/com/Assignment/Four/assignment_four_new/AssignmentFourNewApplication.java
@@ -0,0 +1,13 @@
+package com.Assignment.Four.assignment_four_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentFourNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentFourNewApplication.class, args);
+	}
+
+}
---
+++ SpringDataJPA/assignment_four_new/src/test/java/com/Assignment/Four/assignment_four_new/AssignmentFourNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Assignment.Four.assignment_four_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentFourNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
